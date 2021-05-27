bb8f8af23418f42100a22a722eda2a9a39c64174
LedZeppelin68 <ledzeppelin68@gmail.com>
2021-05-28 00:54:32 +0300

    Initial commit

---
+++ audio checksum generator/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace audio_checksum_generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(794, 84);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(123, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(441, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragOver += new System.Windows.Forms.DragEventHandler(this.textBox1_DragOver);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(581, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 14;
+            this.listBox1.Location = new System.Drawing.Point(3, 273);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(794, 174);
+            this.listBox1.TabIndex = 1;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ audio checksum generator/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using HashLib;
+
+namespace audio_checksum_generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            textBox1.Text = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string working_dir = textBox1.Text;
+
+            string[] flacs = Directory.GetFiles(working_dir, "*.flac", SearchOption.AllDirectories);
+
+            if (flacs.Length != 0)
+            {
+                for (int i = 0; i < flacs.Length; i++)
+                {
+                    string[] cuesheet = File.ReadAllLines(flacs[i].Replace(".flac", ".cue")).Where(x => x.Contains("INDEX 01")).ToArray();
+                    List<int> cue_indexes = cuesheet.Select(x => MFS2Samples(x.Replace("INDEX 01", "").Trim())).ToList();
+
+                    int sample_total = ShowSamples(flacs[i]);
+                    cue_indexes.Add(sample_total);
+
+                    IHash crc32_full = HashFactory.Checksum.CreateCRC32a();
+                    IHash crc32_full_wonull = HashFactory.Checksum.CreateCRC32a();
+                    IHash md5_full = HashFactory.Crypto.CreateMD5();
+                    crc32_full.Initialize();
+                    crc32_full_wonull.Initialize();
+                    md5_full.Initialize();
+
+                    for (int j = 0; j < cue_indexes.Count - 1; j++)
+                    {
+                        IHash crc32_single = HashFactory.Checksum.CreateCRC32a();
+                        IHash crc32_single_wonull = HashFactory.Checksum.CreateCRC32a();
+                        IHash md5_single = HashFactory.Crypto.CreateMD5();
+                        crc32_single.Initialize();
+                        crc32_single_wonull.Initialize();
+                        md5_single.Initialize();
+
+                        int sample_start = cue_indexes[j];
+                        int sample_count = cue_indexes[j + 1] - cue_indexes[j];
+
+                        UInt32 arv1_sum = 0;
+                        UInt32 sample_counter = 1;
+
+                        UInt32 arv2_sum = 0;
+                        UInt32 sample_counter_v2 = 1;
+
+                        using (Process flacexe = new Process())
+                        {
+                            flacexe.StartInfo.FileName = "flac.exe";
+                            flacexe.StartInfo.Arguments = string.Format("--decode --stdout --skip={0} --until=+{1} \"{2}\"", sample_start, sample_count, flacs[i]);
+                            flacexe.StartInfo.UseShellExecute = false;
+                            flacexe.StartInfo.RedirectStandardOutput = true;
+                            flacexe.StartInfo.CreateNoWindow = true;
+
+                            flacexe.Start();
+
+                            using (BinaryReader memr = new BinaryReader(flacexe.StandardOutput.BaseStream))
+                            {
+                                memr.ReadBytes(44);
+
+                                byte[] buffer;
+                                do
+                                {
+                                    buffer = memr.ReadBytes(2352 * 2);
+
+                                    crc32_full.TransformBytes(buffer);
+                                    md5_full.TransformBytes(buffer);
+
+                                    crc32_single.TransformBytes(buffer);
+                                    md5_single.TransformBytes(buffer);
+
+                                    using (BinaryReader bufferr = new BinaryReader(new MemoryStream(buffer)))
+                                    {
+                                        while (bufferr.BaseStream.Position != bufferr.BaseStream.Length)
+                                        {
+                                            Int16 sample = bufferr.ReadInt16();
+                                            if (sample != 0)
+                                            {
+                                                crc32_full_wonull.TransformShort(sample);
+                                                crc32_single_wonull.TransformShort(sample);
+                                            }
+                                        }
+                                    }
+
+                                    using (BinaryReader bufferr = new BinaryReader(new MemoryStream(buffer)))
+                                    {
+                                        while (bufferr.BaseStream.Position != bufferr.BaseStream.Length)
+                                        {
+                                            UInt32 sample = bufferr.ReadUInt32();
+                                            arv1_sum += sample * sample_counter++;
+                                        }
+                                    }
+
+                                    using (BinaryReader bufferr = new BinaryReader(new MemoryStream(buffer)))
+                                    {
+                                        while (bufferr.BaseStream.Position != bufferr.BaseStream.Length)
+                                        {
+                                            UInt64 sample = bufferr.ReadUInt32();
+
+                                            UInt64 _tempcrc = sample * sample_counter_v2++;
+                                            arv2_sum += (UInt32)(_tempcrc / (UInt64)0x100000000);
+                                            arv2_sum += (UInt32)(_tempcrc & (UInt64)0xffffffff);
+                                        }
+                                    }
+                                }
+                                while (buffer.Length == 2352 * 2);
+                            }
+                            flacexe.WaitForExit();
+                        }
+
+                        string crc32_single_result = crc32_single.TransformFinal().GetUInt().ToString("x8");
+                        string crc32_single_wonull_result = crc32_single_wonull.TransformFinal().GetUInt().ToString("x8");
+                        string md5_single_result = BitConverter.ToString(md5_single.TransformFinal().GetBytes()).Replace("-", "").ToLower();
+
+                        string arv1 = arv1_sum.ToString("x8");
+                        string arv2 = arv2_sum.ToString("x8");
+
+                        Invoke((MethodInvoker)delegate
+                        {
+                            listBox1.Items.Add(string.Format("{0:D2} {1,9} {2} {3} {4} {5} {6}", j + 1, sample_count, crc32_single_result, crc32_single_wonull_result, md5_single_result, arv1, arv2));
+                        });
+                    }
+
+                    string crc32_full_result = crc32_full.TransformFinal().GetUInt().ToString("x8");
+                    string crc32_full_wonull_result = crc32_full_wonull.TransformFinal().GetUInt().ToString("x8");
+                    string md5_full_result = BitConverter.ToString(md5_full.TransformFinal().GetBytes()).Replace("-", "").ToLower();
+
+                    Invoke((MethodInvoker)delegate
+                    {
+                        listBox1.Items.Add(string.Empty);
+                        listBox1.Items.Add(string.Format("   {0} {1} {2} {3}", sample_total, crc32_full_result, crc32_full_wonull_result, md5_full_result));
+                    });
+                }
+            }
+        }
+
+        private int ShowSamples(string flac)
+        {
+            int samples = 0;
+            using (Process metaflacexe = new Process())
+            {
+                metaflacexe.StartInfo.FileName = "metaflac.exe";
+                metaflacexe.StartInfo.Arguments = string.Format("--show-total-samples \"{0}\"", flac);
+                metaflacexe.StartInfo.UseShellExecute = false;
+                metaflacexe.StartInfo.RedirectStandardOutput = true;
+                metaflacexe.StartInfo.CreateNoWindow = true;
+
+                metaflacexe.Start();
+                samples = Convert.ToInt32(metaflacexe.StandardOutput.ReadLine());
+                metaflacexe.WaitForExit();
+            }
+
+            return samples;
+        }
+
+        private int MFS2Samples(string v)
+        {
+            string[] MSF = v.Split(new char[] { ':' });
+            int samples = 0;
+
+            samples += Convert.ToInt32(MSF[2]) * 588;
+            samples += Convert.ToInt32(MSF[1]) * 588 * 75;
+            samples += Convert.ToInt32(MSF[0]) * 588 * 75 * 60;
+
+            return samples;
+        }
+    }
+}
